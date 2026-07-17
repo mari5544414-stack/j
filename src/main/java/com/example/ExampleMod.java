4291dd73a3763ebe8f96139739cc2c91d06b06b3
Gamer <mari5544414@gmail.com>
2026-07-17 23:42:18 +0300

    Refactor ExampleMod class and update initialization

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,55 +1,15 @@
-package net.fabricmc.example;
+package com.example;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import org.lwjgl.glfw.GLFW;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ExampleModClient implements ClientModInitializer {
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "modid";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    // Свой ID модификатора вместо старого UUID
-    private static final Identifier SPEED_BOOST_ID =
-            Identifier.of("example-mod", "speed_boost");
-
-    private static KeyBinding speedKey;
-
-    @Override
-    public void onInitializeClient() {
-        speedKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.example-mod.speedboost",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_RIGHT_SHIFT,
-                "category.example-mod.keys"
-        ));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (speedKey.wasPressed()) {
-                if (client.player == null) continue;
-
-                EntityAttributeInstance attribute =
-                        client.player.getAttributeInstance(EntityAttributes.MOVEMENT_SPEED);
-
-                if (attribute == null) continue;
-
-                if (attribute.getModifier(SPEED_BOOST_ID) == null) {
-                    attribute.addTemporaryModifier(new EntityAttributeModifier(
-                            SPEED_BOOST_ID,
-                            100.0,
-                            EntityAttributeModifier.Operation.ADD_VALUE
-                    ));
-                    client.player.sendMessage(Text.literal("§aСкорость +100"), true);
-                } else {
-                    attribute.removeModifier(SPEED_BOOST_ID);
-                    client.player.sendMessage(Text.literal("§cСкорость сброшена"), true);
-                }
-            }
-        });
-    }
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
 }
