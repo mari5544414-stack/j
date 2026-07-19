a24686fbcae504cc9eca30add1bc955b0a6ac3d4
Gamer <mari5544414@gmail.com>
2026-07-19 11:00:43 +0300

    Update ModState.java

--- src/client/java/com/example/client/ModState.java
+++ src/client/java/com/example/client/ModState.java
@@ -3,4 +3,5 @@ package com.example.client;
 public class ModState {
     public static boolean noclipEnabled = false;
     public static boolean flightEnabled = false;
+    public static boolean godModeEnabled = false;
 }
